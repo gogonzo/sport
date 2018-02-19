50eedcf9d85e457b94940e7f822497feea1765c5
Dawid Kałędkowski <dawid.kaledkowski@vemma.eu>
2018-02-19 20:48:39 +0100

    add formula recognition to glicko and glicko2 for example: rank|id ~ rider_name

--- R/dbl_wrapper.R
+++ R/dbl_wrapper.R
@@ -11,29 +11,38 @@
 #' @param r named vector of initial rating estimates. In there is no assumption, initial ratings should be r=1500. Names of vector should correspond with team_name label. 
 #' @param  rd named vector of initial rating deviation estimates. In there is no assumption, initial ratings should be r=300 Names of vector should correspond with team_name label.
 #' @export
-
-glicko_run <- function(data, r, rd){
-  if(missing(r)){
-    team_names <- unique(data$team_name)
+glicko_run <- function(formula, data, r, rd, time){
+  y  <- all.vars(formula)[1]
+  id <- all.vars(formula)[2]
+  x  <- all.vars(formula)[-(1:2)]
+  
+  if( missing(r) ){
+    team_names <- unique(data[[x]])
     r <- setNames( rep(1500, length(team_names)), team_names )
     rd<- setNames( rep(300,  length(team_names)), team_names )
   }
+  if( missing(time) ){
+    data$time <- 0
+    time <- "time"
+  } 
   
   if(any(class(data)=="data.frame"))
-    data <- split(data, data$id)  
+    data <- split( data, data[[ id ]] )  
   
-
+  
   
   glicko_list <- list()
   for(i in 1:length(data)){
-    
-    team_names<- data[[ i ]][["team_name"]]
-    ranks  <- data[[ i ]][["rank"]]
-    time   <- data[[ i ]][["time"]]
-    
-    glicko <- glicko( team_names , rank = ranks, r = r[team_names, ], rd = rd[team_names,], time = time )    
-    r [ team_names, ] <- glicko$r[  team_names ]
-    rd[ team_names, ] <- glicko$rd[ team_names ]
+    team_names <- data[[ i ]][[ x ]]
+    glicko <- glicko( 
+      team_names , 
+      rank = data[[ i ]][[ y ]], 
+      r = r[team_names ], 
+      rd = rd[team_names ], 
+      time = data[[ i ]][[ time ]] 
+    )    
+    r [ team_names ] <- glicko$r[  team_names ]
+    rd[ team_names ] <- glicko$rd[ team_names ]
     
     glicko_list[[ i ]] <- glicko
     
@@ -57,39 +66,46 @@ glicko_run <- function(data, r, rd){
 #' @param  sig named vector of rating volatile. In there is no assumption, initial ratings should be sig=0.5. Names of vector should correspond with team_name label.
 #' @export
 
-glicko2_run <- function(data, r, rd, sig){
-  if(missing(r)){
-    team_names <- unique(data$team_name)
-    r <- setNames( rep(1500, length(team_names)), team_names )
-    rd<- setNames( rep(300,  length(team_names)), team_names )
+glicko2_run <- function(formula, data, r, rd,sig, time){
+  y  <- all.vars(formula)[1]
+  id <- all.vars(formula)[2]
+  x  <- all.vars(formula)[3]
+  
+  if( missing(r) ){
+    team_names <- unique(data[[x]])
+    r   <- setNames( rep(1500, length(team_names)), team_names )
+    rd   <- setNames( rep(300,  length(team_names)), team_names )
+    sig  <- rep(0.05, length(team_names)) %>% setNames(team_names)
   }
-  if(any(class(data)=="data.frame")) data <- split(data, data$id)  
+  if( missing(time) ){
+    data$time <- 0
+    time <- "time"
+  } 
+  
+  if(any(class(data)=="data.frame"))
+    data <- split( data, data[[ id ]] )  
   
   glicko_list <- list()
   for(i in 1:length(data)){
-    
-    team_name <- data[[ i ]][["team_name"]]
-    ranks  <- data[[ i ]][["rank"]]
-    time   <- data[[ i ]][["time"]]
+    team_names <- data[[ i ]][[ x ]]
     
     glicko <- glicko2( 
-      team_name , 
-      rank = ranks, 
-      r    = r[team_name,] ,  
-      rd   = rd[team_name,] , 
-      sig  = sig[team_name,] , 
-      time = time 
+      team_names , 
+      rank = data[[ i ]][[ y ]], 
+      r    = r[ team_names ] ,  
+      rd   = rd[ team_names ] , 
+      sig  = sig[ team_names ] , 
+      time = data[[ i ]][[ time ]]
     )  
-    r [ team_name, ] <- glicko$r[  team_name ]
-    rd[ team_name, ] <- glicko$rd[ team_name ]
-    sig[team_name, ] <- glicko$sig[ team_name ]
+    r [ team_names ] <- glicko$r[  team_names ]
+    rd[ team_names ] <- glicko$rd[ team_names ]
+    sig[ team_names ] <- glicko$sig[ team_names ]
     
     glicko_list[[ i ]] <- glicko
     
   }
   return(glicko_list)
 }
-
 #' BBT rating algorithm
 #' 
 #' BBT rating algorithm
@@ -143,7 +159,7 @@ bbt_run <- function(data, r,rd){
 #'   \item rank position of team_name in event
 #' }  
 #' @param R named vector of initial rating estimates. In there is no assumption, initial ratings should be r=25 Names of vector should correspond with team_name label. 
-#' @param  rd named vector of initial rating deviation estimates. In there is no assumption, initial ratings should be r=25/3 Names of vector should correspond with team_name label.
+#' @param  RD named vector of initial rating deviation estimates. In there is no assumption, initial ratings should be r=25/3 Names of vector should correspond with team_name label.
 #' @export
 
 bdl_run <- function(data,R,RD){
