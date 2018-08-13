4cbde947977bbadded03fd145095594291e9da5d
Dawid Kałędkowski <dawid.kaledkowski@vemma.eu>
2018-08-13 22:18:03 +0200

    add sigma to glicko2 output

--- R/glicko2_run.R
+++ R/glicko2_run.R
@@ -18,7 +18,7 @@ NULL
 #' @param rd named vector of initial rating deviation estimates. In there is no assumption, initial ratings is set to be r=300 Names of vector should correspond with team_name label.
 #' @param sig named vector of rating volatile. In there is no assumption, initial ratings should be sig=0.5. Names of vector should correspond with team_name label.
 #' @param tau The system constant. Which constrains the change in volatility over time. Reasonable choices are between 0.3 and 1.2 (`default = 0.5`), though the system should be tested to decide which value results in greatest predictive accuracy. Smaller values of `tau` prevent the volatility measures from changing by largeamounts, which in turn prevent enormous changes in ratings based on very improbable results. If the application of Glicko-2 is expected to involve extremely improbable collections of game outcomes, then `tau` should be set to a small value, even as small as, say, `tau= 0`.2.
-#' @param weight name of column in `data` containing weights. Weights multiplies step update increasing/decreasing step impact on ratings estimates.
+#' @param weight name of column in `data` containing weights. Weights increasing or decreasing update change. Higher weight increasing impact of corresponding event.
 #' @param date name of column in `data` containing date. Doesn't affect estimation process. If specified, charts displays estimates changes in time instead of by event `id`
 #' @param init_r initial rating for new competitors (contains NA). Default = 1500
 #' @param init_rd initial rating deviations for new competitors. Default = 350
@@ -27,6 +27,7 @@ NULL
 #' \itemize{
 #'   \item \code{final_r} named vector containing players ratings.
 #'   \item \code{final_rd} named vector containing players ratings deviations.
+#'   \item \code{final_sig} named vector containing players ratings volatiles.
 #'   \item \code{r} data.frame with evolution of the ratings and ratings deviations estimated at each event.
 #'   \item \code{pairs} pairwise combinations of players in analysed events with prior probability and result of a challange.
 #'   \item \code{class} of the object
@@ -35,9 +36,9 @@ NULL
 #' }
 #' @export
 
-glicko2_run <- function(formula, data, r, rd,sig, tau, weight, date,init_r = 1500, init_rd=350){
+glicko2_run <- function(formula, data, r, rd,sig, tau, weight, date, init_r = 1500, init_rd=350){
   if(missing(formula)) stop("Formula is not specified")
-  if( length(all.vars(update(formula, .~0)) ) != 2) stop("Left hand side formula must contain two variables")
+  if( !length(all.vars(update(formula, .~0)) )  %in% c(1,2)) stop("Left hand side formula must contain two variables")
   if( length(all.vars(update(formula, 0~.)) ) != 1) stop("Glicko expects only one variable which is ~ 1|pid_variable")
   
   y  <- all.vars(formula)[1]
@@ -47,7 +48,13 @@ glicko2_run <- function(formula, data, r, rd,sig, tau, weight, date,init_r = 150
   if( missing(r) ){
     player_names <- unique(data[[x]])
     r   <- setNames( rep( init_r, length( player_names) ), player_names )
+  }
+  if( missing(rd) ){
+    player_names <- unique(data[[x]])
     rd  <- setNames( rep( init_rd , length( player_names) ), player_names )
+  }
+  if( missing(sig) ){
+    player_names <- unique(data[[x]])
     sig <- setNames( rep( 0.05, length( player_names) ), player_names )
   }
   if( missing(weight) ){
@@ -77,7 +84,7 @@ glicko2_run <- function(formula, data, r, rd,sig, tau, weight, date,init_r = 150
     rd[ player_names ] <- model$rd[ player_names ]
     sig[ player_names ] <- model$sig[ player_names ]
     
-    model_r[[ i ]] <- data.frame(names=player_names, r = model$r, rd = model$rd)
+    model_r[[ i ]] <- data.frame(names=player_names, r = model$r, rd = model$rd, sig = model$sigma)
     model_P[[ i ]] <- model$pairs
     
   }
@@ -99,8 +106,9 @@ glicko2_run <- function(formula, data, r, rd,sig, tau, weight, date,init_r = 150
   out <- structure(
     list(final_r  = r,
          final_rd = rd,
+         final_sig=sig,
          r        = structure( model_r, class="data.frame", identifier = model_r_date),
-         pairs    = structure( model_P, class="data.frame"  , identifier = model_P_date)),
+         pairs    = structure( model_P, class="data.frame", identifier = model_P_date)),
     class="sport",
     method = "glicko2",
     formula = formula
