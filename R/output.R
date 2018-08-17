a4d73d75028e766d65d22717f0a4ddb412c052b5
Dawid Kałędkowski <dawid.kaledkowski@vemma.eu>
2018-08-17 23:33:27 +0200

    describe summary

--- R/output.R
+++ R/output.R
@@ -6,6 +6,22 @@ NULL
 #' Summary for object of class `sport`
 #' @param object of class sport
 #' @param ... optional arguments
+#' @return 
+#' List with following elements \itemize{
+#' \item \code{formula} modelled formula.
+#' \item \code{method} type of algorithm used.
+#' \item \code{Overall Accuracy} named vector containing players ratings.
+#' \item \code{r} data.frame summarized players ratings and model winning probabilities. Probabilities are returned only in models with one variable (ratings) \itemize{
+#' \item \code{name} of a player
+#' \item \code{r} players ratings
+#' \item \code{rd} players ratings deviation
+#' \item \code{`Model probability`} mean predicted probability of winning the challange by the player.
+#' \item \code{`True probability`} mean observed probability of winninh the challange by the player.
+#' \item \code{`Accuracy`} Accuracy of prediction.
+#' \item \code{`pairings`} number of pairwise occurences.
+#' }
+#' }
+#' 
 #' @examples
 #' model <- glicko_run(rank|id~rider, gpheats[1:100,]) 
 #' summary(model)
@@ -27,7 +43,8 @@ summary.sport <- function(object,...){
                                  rd = object$final_rd )
   
 
-  if(attr(object,"method") != "dbl" )
+  
+  if( length(all.vars(update(attr(object,"formula"), 0~.)))==1 )
     r = players_ratings[model_probs_players, on = 'name']  else
     r = players_ratings
   
