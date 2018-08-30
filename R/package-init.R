82a25cb9c2bd7520d1d9def2fb2e9917844d9f44
Dawid Kałędkowski <dawid.kaledkowski@vemma.eu>
2018-08-30 15:49:58 +0200

    add p_win to global variables

--- R/package-init.R
+++ R/package-init.R
@@ -3,4 +3,4 @@
 NULL
 
 if(getRversion() >= "2.15.1")  
-  utils::globalVariables(c(".","P", "Y","r","rd","Interval","name"))
+  utils::globalVariables(c(".","P", "Y","r","rd","Interval","name","p_win"))
