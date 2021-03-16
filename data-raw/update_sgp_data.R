e54ac9c5664e2ebdef8288f613428ba41dce384d
gogonzo <dawid.kaledkowski@gmail.com>
2021-03-16 07:30:38 +0100

    - update SGP data - add Ekstraliga results

--- data-raw/update_sgp_data.R
+++ data-raw/update_sgp_data.R
@@ -10,9 +10,10 @@ update_sgp_data <- function() {
                    password = "Elo#21ok",
                    encoding = "UTF-8")
   dbGetQuery(con, "SET NAMES utf8")
-  gpsquads <- customQuery({
-    "
-    SELECT 
+  gpsquads <- customQuery(
+    con = con, 
+    query = {
+    "SELECT 
       e.id,
       e.season,
       e.date,
@@ -27,9 +28,10 @@ update_sgp_data <- function() {
     WHERE competition = 'Grand-Prix'
     ;"
   })
-  gpheats <- customQuery({
-    "
-    SELECT 
+  gpheats <- customQuery(
+    con = con, 
+    query = {
+    "SELECT 
       e.id,
       e.season,
       e.date,
@@ -73,5 +75,93 @@ update_sgp_data <- function() {
   
   
   usethis::use_data(gpsquads, 
-                   gpheats, overwrite = TRUE)
+                   gpheats, 
+                   overwrite = TRUE, 
+                   compress = "xz")
+}
+
+
+update_league_data <- function() {
+  library(oddsandsods)
+  library(magrittr)
+  library(dplyr)
+  library(RMySQL)
+  library(runner)
+  con <- dbConnect(drv = MySQL(), 
+                   username = "root", 
+                   dbname = "speedway", 
+                   password = "Elo#21ok",
+                   encoding = "UTF-8")
+  dbGetQuery(con, "SET NAMES utf8")
+  plsquads <- customQuery(
+    con = con, 
+    query = {
+      "SELECT 
+      e.id,
+      e.season,
+      e.date,
+      e.place,
+      e.round,
+      e.name, 
+      s.rider_name rider,
+      s.team_name,
+      s.points,
+      s.classification
+    FROM speedway.event_squads s
+    LEFT JOIN speedway.events e on e.id = s.event_id
+    WHERE competition regexp '^PL '
+    ;"
+    })
+  plheats <- customQuery(
+    con = con, 
+    query = {
+      "SELECT 
+      e.id,
+      e.season,
+      e.date,
+      e.round,
+      e.name, 
+      h.heat,
+      h.field,
+      h.rider_name rider,
+      es.team_name,
+      h.points,
+      h.position
+    FROM speedway.event_heats h
+    LEFT JOIN speedway.event_squads es on es.event_id = h.event_id and es.rider_name = h.rider_name
+    LEFT JOIN speedway.events e on e.id = h.event_id
+    WHERE 
+      competition regexp '^PL '
+    "
+    })
+  dbDisconnect(con)
+  
+  plsquads$date <- as.POSIXct(strptime(plsquads$date, "%Y-%m-%d %H:%M:%S"))
+  plheats$date <- as.POSIXct(strptime(plheats$date, "%Y-%m-%d %H:%M:%S"))
+  
+  plheats <-
+    plheats %>%
+    arrange(date, heat) %>%
+    mutate(id = runner::sum_run(as.integer(paste(date, heat) != lag(paste(date, heat), default = "")))) %>%
+    filter(!is.na(position)) %>%
+    filter(!is.na(points)) %>%
+    filter(!position %in% c("F", "N")) %>%
+    filter(!is.na(field)) %>%
+    filter(!is.na(rider)) %>%
+    mutate(
+      rank = as.integer(position),
+      rank = ifelse(is.na(rank), max(rank, na.rm = T) + 1, rank)
+    )
+  
+  Encoding(plheats$name)  <- "UTF-8"
+  Encoding(plheats$rider) <- "UTF-8"
+  Encoding(plsquads$name)  <- "UTF-8"
+  Encoding(plsquads$place) <- "UTF-8"
+  Encoding(plsquads$rider) <- "UTF-8"
+  
+  
+  usethis::use_data(plsquads, 
+                    plheats, 
+                    overwrite = TRUE, 
+                    compress = "xz")
 }
