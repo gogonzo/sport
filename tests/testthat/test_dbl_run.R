24bea73a77795e2510b404dd75de6d3efc55b5c9
go_gonzo <dawid.kaledkowski@gmail.com>
2023-12-24 14:51:03 +0100

    fixes

--- tests/testthat/test_dbl_run.R
+++ tests/testthat/test_dbl_run.R
@@ -3,12 +3,12 @@ context("dbl_run")
 test_that("wrong formula", {
   expect_error(
     dbl_run(
-      formula <- rank | id ~ player(rider|team),
+      formula <- rank | id ~ player(rider | team),
       data = gpheats[1:8, ]
     ),
     "Please specify only one variable inside of the player"
   )
-  
+
   expect_error(
     dbl_run(
       formula <- rank + id ~ player(rider),
@@ -16,7 +16,7 @@ test_that("wrong formula", {
     ),
     "LHS"
   )
-  
+
   expect_error(
     dbl_run(
       formula <- rank | id ~ rider,
@@ -24,7 +24,7 @@ test_that("wrong formula", {
     ),
     "Formula requires specifying player"
   )
-  
+
   expect_warning(
     dbl_run(
       formula <- rank ~ player(rider),
@@ -32,7 +32,7 @@ test_that("wrong formula", {
     ),
     "LHS"
   )
-  
+
   expect_error(
     dbl_run(
       formula <- rank | idd ~ rider,
@@ -40,48 +40,48 @@ test_that("wrong formula", {
     ),
     "idd specified in formula are not present in data"
   )
-  
+
   expect_silent(
     dbl_run(
       formula <- rank | id ~ player(rider),
       data = gpheats[1:8, ]
     )
   )
-  
-  
+
+
   expect_silent(
     dbl_run(
       formula <- rank | id ~ player(rider) + field,
       data = gpheats[1:8, ]
     )
   )
-  
+
   expect_silent(
     dbl_run(
-      formula <- rank | id ~ player(rider) + field*heat + heat:rider,
+      formula <- rank | id ~ player(rider) + field * heat + heat:rider,
       data = gpheats[1:8, ]
     )
   )
 })
 
-test_that("initial",{
+test_that("initial", {
   expect_warning(
     dbl_run(
       formula <- rank | id ~ player(rider),
       data = gpheats[1:8, ],
-      r = setNames(.5, "rider=Tomasz GOLLOB")),
+      r = setNames(.5, "rider=Tomasz GOLLOB")
+    ),
     "Missing parameters will be added with init_r"
   )
-  
+
   expect_warning(
     dbl_run(
       formula <- rank | id ~ player(rider),
       data = gpheats[1:8, ],
-      rd = setNames(.5, "rider=Tomasz GOLLOB")),
+      rd = setNames(.5, "rider=Tomasz GOLLOB")
+    ),
     "Missing parameters will be added with init_rd"
   )
-
-  
 })
 
 test_that("output", {
@@ -93,20 +93,20 @@ test_that("output", {
     init_r = 0,
     init_rd = 1
   )
-  
+
   expect_identical(
     out1$final_r,
     setNames(
-      c(1/3, -1, 1, -1/3),
+      c(1 / 3, -1, 1, -1 / 3),
       c("rider=Tomasz GOLLOB", "rider=Gary HAVELOCK", "rider=Chris LOUIS", "rider=Tony RICKARDSSON")
     )
   )
-  
+
   expect_true(all(out1$r$R == 0))
   expect_true(all(out1$r$RD == 1))
   expect_true(all(out1$pairs$P == 0.5))
-  
-  
+
+
   out2 <- dbl_run(
     formula = formula <- rank | id ~ player(rider),
     data = gpheats[1:4, ],
@@ -115,9 +115,8 @@ test_that("output", {
     init_r = 1,
     init_rd = 2
   )
-  
+
   expect_true(all(out2$r$R == 1))
   expect_true(all(out2$r$RD == 2))
   expect_true(all(out2$pairs$P == 0.5))
 })
-
